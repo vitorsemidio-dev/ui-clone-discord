1e126639e96ed13bd2a96d048ad791ffe6e15cf1
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-05 22:58:55 -0300

    Criação estrutura Channel Info

---
+++ src/components/ChannelInfo/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const HashTagIcon = styled.div``;
+
+export const Title = styled.div``;
+
+export const Separator = styled.div``;
+
+export const Description = styled.div``;
+
