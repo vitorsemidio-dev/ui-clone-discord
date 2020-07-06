ccab200ee72a7d4067e970d97d8ff6c06db2eb83
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-05 23:53:29 -0300

    Estrutura do UserInfo

---
+++ src/components/UserInfo/styles.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { FaMicrophone, FaHeadphones, FaCog } from 'react-icons/fa';
+
+export const Container = styled.div``;
+
+export const Profile = styled.div``;
+
+export const Avatar = styled.div``;
+
+export const UserData = styled.div``;
+
+export const Icons = styled.div``;
+
+export const MicIcon = styled(FaMicrophone)``;
+
+export const HeadphoneIcon = styled(FaHeadphones)``;
+
+export const SettingsIcon = styled(FaCog)``;
